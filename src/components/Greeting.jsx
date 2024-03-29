4de9a5b9ae73a6a8c6927b5767fb3fad89038aed
LadyAudrey <theaudreygreene@gmail.com>
2024-03-29 10:50:44 -0700

    working on MST state. Type error

---
+++ src/components/Greeting.jsx
@@ -0,0 +1,13 @@
+export function Greeting() {
+  return (
+    <>
+      <h1>Welcome to Cadence Clock!</h1>
+      <h2>
+        At Cadence Clock, we believe in empowering you to customize your
+        Pomodoro experience Choose the perfect music playlists to accompany your
+        focus and rest periods
+      </h2>
+      <button>Ready to Start?</button>
+    </>
+  );
+}
